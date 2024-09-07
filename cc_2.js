84b0f2fc00e49a5f76aaaa36c81a51c65be203f6
Alex Cleveland <acleveland2@usf.edu>
2024-09-07 19:02:18 -0400

    Create and test calculate tip function.

--- cc_2.js
+++ cc_2.js
@@ -1,9 +1,18 @@
 // Task 1 Calculate Tip
 
 let billsize = 200;
-let tip = (billsize >= 50 && billsize <= 300) ? billsize * 0.15 : billsize * 0.20;
+let tip = (billsize >= 50 && billsize <= 300)?billsize* 0.15:billsize *.2;
 console.log("Tip:",tip);
 
 // Task 2 Output Details 
 let total = billsize + tip;
 console.log(`The Bill Was $${billsize}, The Tip Was $${tip}, The Total Altogther was $${total}`);
+
+//Task 3 create function
+function calculateTip(billsize) {
+   return (billsize >= 50 && billsize <= 300)?billsize *.15:billsize *.2;
+}
+let anyBill = 100;
+let anyTip = calculateTip(anyBill);
+console.log(`the bill was $${anyBill}, the tip was $${anyTip}, the total altogether $${anyBill + anyTip}`);
+
